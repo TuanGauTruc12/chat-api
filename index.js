81b9a0cd7c709f5411e0552cfd3d83791178ba0c
TuanGauTruc12 <88184136+TuanGauTruc12@users.noreply.github.com>
2023-12-13 08:33:28 +0700

    Update index.js (add cors)

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'X-Requested-With')
